a507a1410ff46780530da2833f32fe304079b0c2
EAB Development Team <xian@eab.co.uk>
2014-12-25 04:33:03 +0800

    Add French translation

---
+++ translations/fre-FR/translation.ts
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+
+<context>
+    <name>design/weather</name>
+    <message>
+        <source>Cloudy</source>
+        <translation>nuageux</translation>
+    </message>
+    <message>
+        <source>Partly Cloudy</source>
+        <translation>partiellement nuageux</translation>
+    </message>
+    <message>
+        <source>Partly Cloudy/Windy</source>
+        <translation>partiellement nuageux/vent</translation>
+    </message>
+    <message>
+        <source>Showers</source>
+        <translation>averses</translation>
+    </message>
+    <message>
+        <source>AM Showers</source>
+        <translation>averses du matin</translation>
+    </message>
+    <message>
+        <source>PM Showers</source>
+        <translation>averses après-midi</translation>
+    </message>
+    <message>
+        <source>AM Clouds/PM Sun</source>
+        <translation>Nuages ​​le matin / après-midi ensoleillé</translation>
+    </message>
+    <message>
+        <source>Light Rain</source>
+        <translation>pluie</translation>
+    </message>
+    <message>
+        <source>AM Light Rain</source>
+        <translation>pluie du matin</translation>
+    </message>
+    <message>
+        <source>PM Light Rain</source>
+        <translation>pluie après-midi</translation>
+    </message>
+    <message>
+        <source>Rain</source>
+        <translation>pluie</translation>
+    </message>
+    <message>
+        <source>Mostly Cloudy</source>
+        <translation>la plupart du temps nuageux</translation>
+    </message>
+    <message>
+        <source>Sunny</source>
+        <translation>ensoleillé</translation>
+    </message>
+    <message>
+        <source>Mostly Sunny</source>
+        <translation>ensoleillé dans l'ensemble</translation>
+    </message>
+    <message>
+        <source>Mostly Sunny/Wind</source>
+        <translation>ensoleillé dans l'ensemble/vent</translation>
+    </message>
+    <message>
+        <source>Fair</source>
+        <translation>équitable</translation>
+    </message>
+</context>
+
+</TS>
