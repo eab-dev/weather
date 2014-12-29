853c426620e0ffa46ec2346c4f6ddedf397a1862
EAB Development Team <xian@eab.co.uk>
2014-12-29 23:39:27 +0800

    Add Chinese translation

---
+++ translations/chi-CN/translation.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+
+<context>
+    <name>design/weather</name>
+    <message>
+        <source>Cloudy</source>
+        <translation>阴天</translation>
+    </message>
+    <message>
+        <source>Partly Cloudy</source>
+        <translation>少云</translation>
+    </message>
+    <message>
+        <source>Partly Cloudy/Windy</source>
+        <translation>少云/风</translation>
+    </message>
+    <message>
+        <source>Showers</source>
+        <translation>阵雨</translation>
+    </message>
+    <message>
+        <source>AM Showers</source>
+        <translation>上午阵雨</translation>
+    </message>
+    <message>
+        <source>PM Showers</source>
+        <translation>下午阵雨</translation>
+    </message>
+    <message>
+        <source>AM Clouds/PM Sun</source>
+        <translation>多云转晴</translation>
+    </message>
+    <message>
+        <source>Light Rain</source>
+        <translation>小雨</translation>
+    </message>
+    <message>
+        <source>AM Light Rain</source>
+        <translation>上午小雨</translation>
+    </message>
+    <message>
+        <source>PM Light Rain</source>
+        <translation>下午小雨</translation>
+    </message>
+    <message>
+        <source>Rain</source>
+        <translation>雨</translation>
+    </message>
+    <message>
+        <source>Mostly Cloudy</source>
+        <translation>多云</translation>
+    </message>
+    <message>
+        <source>Sunny</source>
+        <translation>晴</translation>
+    </message>
+    <message>
+        <source>Mostly Sunny</source>
+        <translation>晴天</translation>
+    </message>
+    <message>
+        <source>Mostly Sunny/Wind</source>
+        <translation>晴天/有风</translation>
+    </message>
+    <message>
+        <source>Fair</source>
+        <translation>晴天好</translation>
+    </message>
+    <message>
+        <source>Clear</source>
+        <translation>晴朗</translation>
+    </message>
+    <message>
+        <source>Snow</source>
+        <translation>雪</translation>
+    </message>
+    <message>
+        <source>Haze</source>
+        <translation>雾霾</translation>
+    </message>
+</context>
+
+</TS>
